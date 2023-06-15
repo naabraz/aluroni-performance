e1fadb6d1759fd01c3ec707c3b02994a36791212
Natalia Braz <naabraz@gmail.com>
2023-06-15 09:54:22 -0300

    add memo

--- src/pages/Cardapio/Filtros/index.tsx
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -1,31 +1,37 @@
-import React from 'react';
-import filtros from './filtros.json';
-import styles from './Filtros.module.scss';
+import React, { memo } from 'react';
 import classNames from 'classnames';
 
-type IOpcao = typeof filtros[0];
+import filtros from './filtros.json';
+import styles from './Filtros.module.scss';
+
+type IOpcao = (typeof filtros)[0];
 
 interface Props {
   filtro: number | null;
-  setFiltro: React.Dispatch<React.SetStateAction<number | null>>
+  setFiltro: React.Dispatch<React.SetStateAction<number | null>>;
 }
 
-export default function Filtros({ filtro, setFiltro }: Props) {
-
+function Filtros({ filtro, setFiltro }: Props) {
   function selecionarFiltro(opcao: IOpcao) {
     if (filtro === opcao.id) return setFiltro(null);
     return setFiltro(opcao.id);
   }
   return (
     <div className={styles.filtros}>
-      {filtros.map(opcao => (
-        <button className={classNames({
-          [styles.filtros__filtro]: true,
-          [styles['filtros__filtro--ativo']]: filtro === opcao.id
-        })} key={opcao.id} onClick={() => selecionarFiltro(opcao)}>
+      {filtros.map((opcao) => (
+        <button
+          className={classNames({
+            [styles.filtros__filtro]: true,
+            [styles['filtros__filtro--ativo']]: filtro === opcao.id,
+          })}
+          key={opcao.id}
+          onClick={() => selecionarFiltro(opcao)}
+        >
           {opcao.label}
         </button>
       ))}
     </div>
   );
 }
+
+export default memo(Filtros);
--- src/pages/Cardapio/Ordenador/index.tsx
+++ src/pages/Cardapio/Ordenador/index.tsx
@@ -1,37 +1,46 @@
-import styles from './Ordenador.module.scss';
-import opcoes from './opcoes.json';
-import React, { useState } from 'react';
+import React, { memo, useState } from 'react';
 import classNames from 'classnames';
 import { MdKeyboardArrowUp, MdKeyboardArrowDown } from 'react-icons/md';
 
-interface Props { 
-  ordenador: string,
-  setOrdenador: React.Dispatch<React.SetStateAction<string>>
+import styles from './Ordenador.module.scss';
+import opcoes from './opcoes.json';
+
+interface Props {
+  ordenador: string;
+  setOrdenador: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export default function Ordenador({
-  ordenador,
-  setOrdenador
-}: Props) {
+function Ordenador({ ordenador, setOrdenador }: Props) {
   const [aberto, setAberto] = useState(false);
-  const nomeOrdenador = ordenador && opcoes.find(opcao => opcao.value === ordenador)?.nome;
+  const nomeOrdenador =
+    ordenador && opcoes.find((opcao) => opcao.value === ordenador)?.nome;
   return (
     <button
       className={classNames({
         [styles.ordenador]: true,
-        [styles['ordenador--ativo']]: ordenador !== ''
+        [styles['ordenador--ativo']]: ordenador !== '',
       })}
       onClick={() => setAberto(!aberto)}
       onBlur={() => setAberto(false)}
     >
       <span>{nomeOrdenador || 'Ordenar Por'}</span>
-      {aberto ? <MdKeyboardArrowUp size={20} /> : <MdKeyboardArrowDown size={20} /> }
-      <div className={classNames({
-        [styles.ordenador__options]: true,
-        [styles['ordenador__options--ativo']]: aberto
-      })}>
-        {opcoes.map(opcao => (
-          <div className={styles.ordenador__option} key={opcao.value} onClick={() => setOrdenador(opcao.value)}>
+      {aberto ? (
+        <MdKeyboardArrowUp size={20} />
+      ) : (
+        <MdKeyboardArrowDown size={20} />
+      )}
+      <div
+        className={classNames({
+          [styles.ordenador__options]: true,
+          [styles['ordenador__options--ativo']]: aberto,
+        })}
+      >
+        {opcoes.map((opcao) => (
+          <div
+            className={styles.ordenador__option}
+            key={opcao.value}
+            onClick={() => setOrdenador(opcao.value)}
+          >
             {opcao.nome}
           </div>
         ))}
@@ -39,3 +48,5 @@ export default function Ordenador({
     </button>
   );
 }
+
+export default memo(Ordenador);
